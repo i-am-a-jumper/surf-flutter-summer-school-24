e911328392a1b49191332369e250ebc5c5f1947e
Vanya <parandy.ivan@yandex.ru>
2024-07-24 11:26:07 +0300

    day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:surf_flutter_summer_school_24/screens/grid_screen.dart';
+
+import 'screens/slider_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +13,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+      home: GridScreen()
     );
   }
 }
+
+class PageViewExample extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> pages = [
+      Center(
+        child: Image.asset("assets/images/Forest.png"),
+      ),
+      Center(
+        child: Image.asset("assets/images/sea.png"),
+      ),
+      Center(
+        child: Image.asset("assets/images/gt.png"),
+      )
+    ];
+
+    final PageController controller = PageController();
+
+    return PageView(
+      controller: controller,
+      children: pages,
+    );
+  }
+  }
+
+
